0e221819b4fba75cd211f18a5075462663c63fc7
Jose Jimenez <jljb1990@gmail.com>
2015-01-17 17:25:58 -0430

    Terminado el problema 1 a falta de pruebas

--- Proyecto1/1.cpp
+++ Proyecto1/1.cpp
@@ -1,14 +1,53 @@
+/* 
+ * Solucion al problema de repartir M modens en una red de N oficinas
+ * Autores: Arturo Voltattorni y Jose Luis Jimenez
+ *
+ */
+
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
-#include <utility>
 #include <math.h>
 #include <algorithm>
 
 using namespace std;
 
 #define CANT_VALORES 5
+
+class UnionFind 
+{
+  private:
+    vector<int> arrPadres;
+
+  public:
+    UnionFind(int cantNodos)
+    { 
+      arrPadres = *(new vector<int>(cantNodos));
+      for (int i = 0; i < cantNodos; i++) arrPadres[i] = i; 
+    }
+    
+    int find(int nodo) 
+    { 
+      int padre = arrPadres[nodo];
+      if(padre == nodo) return padre;
+      else return find(padre);
+    }
+
+    int join(int izq, int der)
+    {
+      int liderIzq = find(izq);
+      int liderDer = find(der);
+      arrPadres[liderDer] = liderIzq;
+    }
+
+    void print()
+    {
+      cout << endl;
+      for(vector<int>::iterator it = arrPadres.begin(); it != arrPadres.end(); it++) cout << *it << " ";
+      cout << endl;
+    }
+};
 
 struct RegValores
 {
@@ -19,58 +58,17 @@ struct RegValores
   float v;
 };
 
-struct nodo 
+struct NodoDist 
 {
-  // Cantidad de nodos que estan a distancia con costo U
-  int cu;
-  // Nodo actual
-  int nod;
-  vector<nodo>* ady;
-
+  int   izq;  //Nodo de partida del vertice
+  int   der;  //Nodo de llegada del vertice
+  float dist; //Distancia entre ambos nodos
 };
 
 //Funcion que compara nodos para hacer el ordenamiento
-bool compararNodos(const nodo &n1, const nodo &n2)
+bool compararNodos(const NodoDist &n1, const NodoDist &n2)
 {
-  return n1.cu > n2.cu;
-}
-
-// Funcion que crea e inicializa una estructura para cada punto.
-vector<nodo> initNodos(int cantN)
-{
-  vector<nodo> vn;
-  nodo n;
-  for (int i = 0; i < cantN; i++)
-  { 
-    n.cu = 0;
-    n.nod = i;
-    n.ady = new vector<nodo>();
-    vn.push_back(n);
-  }
-
-  return vn;
-}
-
-/* 
-   Funcion que toma el vector de nodos, y para cada nodo guarda la cantidad
-   de nodos cuya distancia tiene costo U y cuales son. 
-*/
-
-void contarDistancias(vector<nodo> &vn, vector<float> vp, int n, int u)
-{
-  int acc = 0;
-  int  in = 0;
-  for(int i = 1; i < n; i++)
-  {
-    int k = i;
-    while (in <= i * n - i - acc - 1)
-    {
-      if(vp[in] <= u) { vn[i-1].cu++; vn[i-1].ady->push_back(vn[k]); vn[k].cu++; vn[k].ady->push_back(vn[i-1]);} 
-      in++;
-      k++;
-    }
-    acc = acc + i;
-  }
+  return n1.dist < n2.dist;
 }
 
 
@@ -99,8 +97,6 @@ RegValores getValores()
   stringstream(valores.at(3)) >> r.u;
   stringstream(entrada) >> r.v;
 
-  cout << "ALALA " << r.n << valores.at(1) << r.m << r.u << r.v << endl;
-
   return r;
 }
 
@@ -128,54 +124,24 @@ vector< pair<int,int> > obPuntos(int n)
 
 // Obtener el vector de las distancias.
 // Vector de la forma < d(1,2),..,d(1,n),d(2,3),..,d(2,n),...d(n-1,n) >
-
-vector<float> obtenerDist(vector< pair<int,int> > vp, int n)
+vector<NodoDist> obtenerDist(vector< pair<int,int> > vp, int n)
 {
-    // Vector que almacena las distancias
-    vector<float> vd;
+                         
+    vector<NodoDist> vd; // Vector que almacena las distancias
+    NodoDist nd;
 
     // Cálculo y almacenamiento de distancias, costo amortizado.
     for (int i = 0; i < n-1; i++)
     {
         for (int j = i+1; j < n; j++)
         {
-            vd.push_back(sqrt(pow(vp[i].first-vp[j].first,2) + pow(vp[i].second-vp[j].second,2)));
+            nd.izq = i;
+            nd.der = j;
+            nd.dist = sqrt(pow(vp[i].first-vp[j].first,2) + pow(vp[i].second-vp[j].second,2));
+            vd.push_back(nd);
         }
-       
     }
-
     return vd;
-
-}
-
-// Funcion que selecciona el nodo a asignar modem y conectar.
-pair<nodo,bool> seleccionarCandidato(vector< nodo > vn, vector<bool> conectados)
-{
-  pair<nodo,bool> res;
-  for( vector< nodo >::const_iterator i = vn.begin(); i != vn.end(); ++i)
-  {
-    if (!conectados[(*i).nod]) { res.first=*i; res.second=true; return res;  }
-  }
-
-  res.second = false;
-
-  return res;
-
-}
-
-// Funcion que conecta recursivamente.
-int conectarNodos(nodo n, vector<bool> &conectados)
-{
-  conectados[n.nod] = true;
-  int c = 1;
-
-  for (vector< nodo >::const_iterator i = n.ady->begin(); i != n.ady->end(); ++i)
-  {
-    if (!conectados[(*i).nod]) { c += conectarNodos(*i,conectados); }
-  }
-
-  return c;
-
 }
 
 int main()
@@ -183,72 +149,50 @@ int main()
   string scasos;
   int cantCasos;
   RegValores v;
-  // Lista de nodos
-  vector< nodo > vn;
-  // Vector que almacena los puntos
-  vector< pair<int,int> > vp;
-  // Vector que almacena las distancias entre los puntos.
-  vector< float > vd;
+                      
+  vector< pair<int,int> > vp;   // Vector que almacena los puntos
+  vector< NodoDist > vd;        // Vector que almacena las distancias entre los puntos.
 
 
-  getline(cin, scasos); stringstream(scasos) >> cantCasos;
+  getline(cin, scasos); stringstream(scasos) >> cantCasos; //Obtenemos la cantidad total de casos
 
   for(int h = 0; h < cantCasos; h++)
   {
-    v = getValores();
-    vp = obPuntos(v.n);
-    vd = obtenerDist(vp,v.n);
+    v = getValores();     //Obtenemos las cantidades n, m, r, u, v
+    vp = obPuntos(v.n);   //Leemos la lista de pares de puntos
+    UnionFind uf(v.n);    //Creamos una estructura union-find para llevar registro de las componentes conexas
 
-    vn = initNodos(v.n);
+    vd = obtenerDist(vp,v.n); //Calculamos todas las distancias
     
-    for( vector<float>::const_iterator i = vd.begin(); i != vd.end(); ++i)
-        cout << *i << ' ';
-    cout << endl;
+    sort(vd.begin(), vd.end(), compararNodos); //Ordenamos los nodos de menor a mayor con respecto a las distancias.
 
-    // Se cuentan las distancias entre puntos y se genera el vector
-    // con la informacion de cada nodo.
-    contarDistancias(vn, vd, v.n, v.r);  
-    for(int i = 0; i < v.n; i++) { cout << vn[i].nod << " " << vn[i].cu << " | "; 
-				   for(vector<nodo>::iterator it = vn[i].ady->begin(); it != vn[i].ady->end(); it++) cout << (*it).nod << " ";
-			           cout << endl;
-				 }
-
-    // Se ordena el vector de nodos de acuerdo a la cantidad de nodos adyacentes
-    // con costo U
-
-    cout << "Antes/Despues del sort" << endl;
-
-    sort(vn.begin(), vn.end(), compararNodos);
-
-    for(int i = 0; i < v.n; i++) { cout << vn[i].nod << " " << vn[i].cu << " | "; 
-				   for(vector<nodo>::iterator it = vn[i].ady->begin(); it != vn[i].ady->end(); it++) cout << (*it).nod << " ";
-			           cout << endl;
-				 }
-
-    cout << "-----------------------------------------" << endl;
-
-    // Arreglo que contiene el estado de conexion de los nodos
-    vector< bool > conectados(v.n,false);
-    pair< nodo, bool > tnb;
-    int cantU = 0;
-    float precioU = 0.0, precioV = 0.0;
-    int cm;
-    // Asignar modem y conectar recursivamente.
-    for(cm = 0; cm < v.m; cm++)
+    // Aplcamos el algoritmos de Kruskal para buscar el arbol minimo cobertor, guardamos las distancias de los 
+    // vertices seleccionados en el arreglo vecDist
+    float vecDist[v.n - 1];
+    int k = 0;
+    for(vector<NodoDist>::const_iterator itd = vd.begin(); itd != vd.end(); itd++)
     {
-      tnb = seleccionarCandidato(vn,conectados);
-
-      if (tnb.second) cantU += conectarNodos(tnb.first,conectados);
-      else break; 
+      NodoDist temp = *itd;
+      if(uf.find(temp.izq) != uf.find(temp.der)) 
+      { 
+        uf.join(temp.izq, temp.der);
+        vecDist[k++] = temp.dist;
+        if(k == v.n - 1) break; 
+      }
     }
-    cout << "CU " << v.n << v.r << v.m << v.u << v.v << endl; 
-    if (cantU!=v.n) {
-        precioV = (v.n-cantU)*(v.v);
-    }
-    
-    // Se restan las oficinas que tienen modem
-    precioU = (cantU-v.m)*v.u;
 
-    cout << "Caso #" << h << ": "<< precioU << " " << precioV << endl;
+    //Calculamos el costo por metro de usar los cables.
+    float precioU = 0.0;
+    float precioV = 0.0;
+
+    for(int i = 0; i < v.n - v.m; i++)
+    {
+      if(vecDist[i] <= v.r) precioU += vecDist[i] * v.u;
+      else precioV += vecDist[i] * v.v;
+    }
+
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(3);
+    cout << "Caso #" << h << ": " << precioU << " " << precioV << endl; 
   }
 }
