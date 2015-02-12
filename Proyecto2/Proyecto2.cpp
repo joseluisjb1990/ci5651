bf3de7d7485ff01a639d7631fd60b06c392c5893
Jose Jimenez <jljb1990@gmail.com>
2015-02-11 21:22:02 -0430

    Terminado, asi como que tu digas AHHGGG terminado, no

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include "grafo.cpp"
 
-Grafo* leerArchivo()
+Grafo* leerArchivo(string nombreArch)
 {
   string linea;
   ifstream archivo;
-  archivo.open("DSJC125.1.col");
+  archivo.open(nombreArch);
 
   Grafo* g = new Grafo();
 
@@ -31,30 +31,8 @@ Grafo* leerArchivo()
   return g;
 }
 
-int main ()
+int main (int argc, char* argv[])
 {
-  cout<< "HOLAAAAAAAAAAAAAA" << endl;
-
-  //Grafo* g = leerArchivo();
-
-  Grafo* g = new Grafo();
-
-  g->agregarLado(1,2);
-  g->agregarLado(1,3);
-  g->agregarLado(2,4);
-  g->agregarLado(2,3);
-  g->agregarLado(3,4);
-  g->agregarLado(2,8);
-  g->agregarLado(8,6);
-  g->agregarLado(4,8);
-  g->agregarLado(4,6);
-  g->agregarLado(6,7);
-  g->agregarLado(7,9);
-  g->agregarLado(4,9);
-  g->agregarLado(2,9);
-  g->agregarLado(7,5);
-  g->print();
-  g->maximalClique();
-  g->Dsatur();
-  g->argoritmo();
+  Grafo* g = leerArchivo(argv[1]);
+  cout << g->Dsatur() << " " << g->brelazModificado() << endl; 
 }
--- Proyecto2/grafo.cpp
+++ Proyecto2/grafo.cpp
@@ -23,18 +23,6 @@ class Grafo
       listAdy[destino].push_back(partida);
     }
     
-    void print()
-    {
-      cout << endl;
-      for(map< int,vector<int> >::iterator it = listAdy.begin(); it != listAdy.end(); it++)
-      {
-        cout << it->first << ": ";
-        for(vector<int>::iterator itv = (it->second).begin(); itv != (it->second).end(); itv++)
-          cout << *itv << " ";
-        cout << endl;
-      }
-    }
-
     vector< pair<int, int> >* sortByDegree()
     {
       vector< pair<int, int> >* vm = new vector< pair<int, int> >();
@@ -127,48 +115,32 @@ class Grafo
             colorAdy[*it].push_back(colorAct);
 
       }
-
-      cout << endl;
-      for(map<int, vector<int> >::iterator it = colorAdy.begin(); it != colorAdy.end(); it++)
-      {
-        cout << (*it).first << ": ";
-        for(vector<int>::iterator itv = (*it).second.begin(); itv != (*it).second.end(); itv++)
-        {
-          cout << *itv << " ";
-        }
-        cout << endl;
-      }
-      
-      for(map<int, int >::iterator it = nodoColor.begin(); it != nodoColor.end(); it++)
-      {
-        cout << (*it).first << ": " << (*it).second << endl;
-      }
-      
-      for(vector< pair< int,int > >::iterator it = vm->begin(); it != vm->end(); it++)
-      {
-        cout << (*it).first << " " << (*it).second << endl;
-      }
     
       return color;
     }
 
-    int argoritmo()
+    int brelazModificado()
     {
       vector<int>* maxClique = maximalClique();
       vector< pair<int, int> >* vm = sortByDegree();
                 
-      int   arrK[vm->size() + 1]; //Arreglo de nodos ordenados por k
-      int   solsParcial[vm->size() + 1]; //Cantidad de colores en la solucion parcial k-1
+      int   arrK[vm->size() + 1];        // Arreglo de indice k y entrada etiqueta de nodo
+      int   rank[vm->size() + 1];        // Arreglo de indice etiqueta de nodo y entrada k
+      int   solsParcial[vm->size() + 1]; // Cantidad de colores en la solucion parcial k-1
       bool  labeled[vm->size() + 1];
       int w = maxClique->size();
       int q = Dsatur();
       vector<int>* Uk[vm->size() + 1];
       int colores[vm->size() + 1][q];
-      int[vm->size() + 1] nodoColor;
+      int nodoColor[vm->size() + 1] ;     // Arreglo que tiene para cada nodo (k-indx) su color
       
+      // Matriz de colores, las filas son nodos y las columnas colores disponibles
+      // para ese nodo. el nodoColor representa el color de ese nodo.
+      // Tanto esta matriz, como el nodoColor es k-indexado.
       for(int i = 1; i <= vm->size(); i++)
       {
-        for(int j = 1; j <= q; j++)
+      	colores[i][0] = 0;
+        for(int j = 1; j < q; j++)
           colores[i][j] = 1;
 
         nodoColor[i] = 0;
@@ -177,8 +149,8 @@ class Grafo
       for(int i = 1; i <= maxClique->size(); i++)
       {
         arrK[i] = maxClique->at(i-1);
+        rank[maxClique->at(i-1)] = i;
         labeled[i] = true;
-        Uk[i] = new vector<int>();
         solsParcial[i] = w;
         nodoColor[i] = i;
       }
@@ -189,6 +161,7 @@ class Grafo
         if(find(maxClique->begin(), maxClique->end(), it->first) == maxClique->end())
         {
           arrK[i]     = it->first;
+          rank[it->first] = i;
           labeled[i]  = false;
           Uk[i++] = new vector<int>();
         }
@@ -197,30 +170,139 @@ class Grafo
 
 
       int color = 1;      
-      int rank[vm->size()];     // Diccionario de nodo a rango
+
 
       int n = vm->size();
-      int s;
       bool back = false;
       int k = w + 1;
-       for()
       
-      while(false)
+      while(true)
       {
         if(!back)
         {
+          int min = 0;    
           int uk = solsParcial[k-1];
-          int min;
           if(uk + 1 < q - 1) min = uk + 1;
-          else              min = q - 1;
+          else               min = q - 1;
 
-          listAdy[arrK[k]]
+          // Recorro el vector de nodos adyacentes, y busco el color de cada uno
+          // para eliminarlo de la matriz de colores del nodo de rango k.
+          vector<int> ady = listAdy[arrK[k]];
+
+          // Devuelvo la fila correspondiente al nodo k a 1, para determinar
+          // el conjunto de colores disponibles.
+          for ( int p = 1; p <= min; p++)
+	      	colores[k][p] = 1;
+
+          // Para cada nodo adyacente, eliminar su color del conjunto de colores
+          // disponibles para el nodo de rango k.
+          for( vector<int>::iterator it = ady.begin(); it != ady.end(); it++)
+          {
+          //  	if (rank[(*it)] < k) colores[k][nodoColor[rank[(*it)]]] = 0; Ciclo infinito con esta linea sin comentario
+                colores[k][nodoColor[rank[(*it)]]] = 0;
+          }
+
+          for ( int e = min+1; e < q; e++)
+	      	colores[k][e] = 0;
+
+	      nodoColor[k] = 0;
 
         } else
         {
-
+          // PENDIENTE ACA QUE NO SE "DESPINTA", NO ESTOY ASIGNANDO A NODOCOLOR 0.
+          colores[k][nodoColor[k]] = 0;
+          labeled[k] = false;
         }
-        // si no back uk es el numero de colores en la solucion parcial 
+
+        // Segundo condicional, verificar si hay colores disponibles
+        bool hayColor = false;
+        int colorSeleccionado = 0;
+        for (int i = 1; i < q; i++)
+        {
+            if (colores[k][i] != 0)
+            {
+            	hayColor = true;
+                colorSeleccionado = i;
+                break;
+            }
+        }
+
+        if (hayColor)
+        {
+            nodoColor[k] = colorSeleccionado;
+
+            // Si colorSeleccionado es mayor, quiere decir que use un nuevo
+            // color, y aumenta la cantidad de colores usados.
+
+          	if (colorSeleccionado > solsParcial[k-1]) 
+          		solsParcial[k] = solsParcial[k-1]+1;
+          	else solsParcial[k] = solsParcial[k-1];
+
+            k++;
+
+            if (k>n) {
+              q = solsParcial[k];
+              // Retornar q si se llega a la menor solucion posible.
+              if (q == w)
+                return q;
+
+              // Recorrer el arreglo de colores hasta encontrar el primer
+              // nodo con el color q.
+              int minK_colorQ = 0;
+              for (int i = 1; i < vm->size() + 1; i++)
+              {
+                if (nodoColor[i] == q)
+                {
+                  minK_colorQ = i;
+                  break;
+                }
+              }
+
+              for (int i = minK_colorQ; i < vm->size() + 1; i++) {
+                labeled[i] = false;
+              }
+
+              k = minK_colorQ;
+              back = true;
+            } else
+            {
+              back = false;
+            }
+
+        } else
+        {
+          back = true;
+        }
+
+	    if(back)
+	    {
+	      vector<int> ady = listAdy[arrK[k]];
+	      bool candidato;
+	      for(vector<int>::iterator it = ady.begin(); it != ady.end(); it++)
+	      {
+	      	candidato = true;
+	      	int adyActual = *it;
+	      	int rankAdy = rank[adyActual];
+	      	if(rankAdy < k)
+	      	{
+	      		int colAdy = nodoColor[rankAdy];
+	      		
+	      		for(int i = 1; i < rankAdy; i++)
+	      		{
+	      			if(find(ady.begin(), ady.end(), arrK[i]) != ady.end())
+	      				if(nodoColor[i] == colAdy) { candidato = false; break; }
+	      		}
+	      	} else
+	      		candidato = false;
+
+	      	if(candidato) labeled[rankAdy] = true;
+	      }
+
+	      for(int i = 1; i <= n; i++)
+	      	if(labeled[i]) k = i;
+
+	      if(k < w) {  return q; }
+        }
       }
     }
-};  
+}; 
